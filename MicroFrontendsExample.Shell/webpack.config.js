3a8528c32873fcce8347d5df4aa12b7df3ffa110
Gayle Angeles <gayleangeles@gmail.com>
2023-01-04 11:18:17 -0700

    initiail commit

---
+++ MicroFrontendsExample.Shell/webpack.config.js
@@ -0,0 +1,24 @@
+﻿const path = require('path');
+const fs = require("fs");
+
+module.exports = [
+    {
+        name: "get-react",
+        entry: () => fs.readdirSync("../ReactMicroFrontend/wwwroot/dist/").filter(f => f.endsWith(".js")).map(f => `../ReactMicroFrontend/wwwroot/dist/${f}`),
+        devtool: "source-map",
+        mode: "development",
+        output: {
+            filename: "app.js",
+            path: path.resolve(__dirname, "./wwwroot/dist/react/")
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    enforce: "pre",
+                    use: ["source-map-loader"]
+                }
+            ]
+        }
+    }
+]
